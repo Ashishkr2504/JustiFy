881abee7083f881e56c753643ae2753fb790461a
Ashish Kumar <ashishkr01062003@gmail.com>
2025-05-22 22:37:43 +0530

    updated mlmodel url in backend

--- backend/src/controllers/documentSearch.controller.ts
+++ backend/src/controllers/documentSearch.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 
+const mlUrl = process.env.ML_SERVICE_URL || 'http://localhost:5001';
+
 export const searchDocuments = async (req: Request, res: Response) => {
   console.log('Received /api/document/search request'); // Add this
   const { query, filter } = req.body;
   try {
-    const pyRes = await axios.post('http://localhost:5001/search', { query, filter });
+    const pyRes = await axios.post(`${mlUrl}/search`, { query, filter });
     res.json(pyRes.data);
   } catch (err) {
     console.error('Error in searchDocuments:', err); // Add this
